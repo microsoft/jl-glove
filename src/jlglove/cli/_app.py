b68a3d68fecb85d0ad2cd25fef889cc902917034
Lorenzo Pisani <lopisani@microsoft.com>
2025-02-26 14:06:29 -0800

    quick setup of 3 cli commands

--- src/jlglove/cli/_app.py
+++ src/jlglove/cli/_app.py
@@ -1,2 +1,24 @@
+import click
+from rats import apps, cli
+
+
+class Application(apps.Container, cli.Container, apps.PluginMixin):
+
+    def execute(self) -> None:
+        cli.create_group(click.Group(), self)()
+
+    @cli.command()
+    def prepare(self) -> None:
+        print("preparing data")
+
+    @cli.command()
+    def train(self) -> None:
+        print("training model")
+
+    @cli.command()
+    def infer(self) -> None:
+        print("infer labels")
+
+
 def main() -> None:
-    print("hello, world")
+    apps.run_plugin(Application)
