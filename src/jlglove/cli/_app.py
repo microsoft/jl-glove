dae53d3c9cd516791d81d71a72d92867adf833b9
Lorenzo Pisani <lopisani@microsoft.com>
2025-02-26 17:56:15 -0800

    make up some initial cli arguments to show as an example

--- src/jlglove/cli/_app.py
+++ src/jlglove/cli/_app.py
@@ -8,16 +8,36 @@ class Application(apps.Container, cli.Container, apps.PluginMixin):
         cli.create_group(click.Group(), self)()
 
     @cli.command()
-    def prepare(self) -> None:
-        print("preparing data")
+    @click.option("--output-path", help="path to store generated training data")
+    def _generate(self, output_path: str) -> None:
+        """Create a directory of generated synthetic data."""
+        print(f"generating synthetic data into: {output_path}")
 
     @cli.command()
-    def train(self) -> None:
-        print("training model")
+    @click.option("--input-path", help="path to raw tcr data")
+    @click.option("--output-path", help="path to store generated training data")
+    def _prepare(self, input_path: str, output_path: str) -> None:
+        """Prepare train/test data from a directory of input tcr data."""
+        print(f"processing data from: {input_path}")
+        print(f"storing prepared data into: {output_path}")
 
     @cli.command()
-    def infer(self) -> None:
-        print("infer labels")
+    @click.option("--input-path", help="path to training data directory")
+    @click.option("--output-path", help="path to store generated training data")
+    def _train(self, input_path: str, output_path: str) -> None:
+        """Train model embeddings from a prepared directory of tcr data."""
+        print(f"training model from: {input_path}")
+        print(f"storing embeddings into: {output_path}")
+
+    @cli.command()
+    @click.option("--model-path", help="path to trained model embeddings")
+    @click.option("--input-path", help="path to unlabeled data")
+    @click.option("--output-path", help="path to store predicted labels")
+    def _infer(self, model_path: str, input_path: str, output_path: str) -> None:
+        """Load trained model embeddings and run inference on a directory of unlabeled data."""
+        print(f"loading model embeddings from: {model_path}")
+        print(f"running inference on input data: {model_path}")
+        print(f"storing predicted labels into: {output_path}")
 
 
 def main() -> None:
