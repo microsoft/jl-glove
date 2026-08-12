7ec5fd7eab32473b1c2437ed40d16fcd4de1e7db
Ash <ashgirlchapfuwa@gmail.com>
2026-08-12 00:02:36 +0000

    Add Azure Artifacts feed and devcontainer infra

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -7,5 +7,9 @@ mkdir -p ~/.config/direnv/
 cp /workspaces/jl-glove/.devcontainer/direnvrc ~/.config/direnv/direnvrc
 
 direnv allow .
+export PIP_INDEX_URL="https://packagefeedproxy.microsoft.io/pypi/simple/"
+pip config set global.index-url "https://packagefeedproxy.microsoft.io/pypi/simple/"
 poetry config virtualenvs.in-project true
+poetry config repositories.pypi https://packagefeedproxy.microsoft.io/pypi/simple/
+poetry lock
 poetry install
